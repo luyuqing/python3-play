d5a17f29445e4943bc89db383f45f3944b7ae117
Yuqing <yuqing@boost.ai>
2020-07-01 09:40:21 +0200

    Added typehints

---
+++ typehints/test.py
@@ -0,0 +1,62 @@
+from typing import Iterable, Tuple, Sequence, Optional, Union, List, Set, TypeVar, Generic
+
+
+# 1
+def test(n: Optional[str]):
+    return 5
+
+
+test(None)
+
+
+# 2
+X = TypeVar('X')
+
+
+def flatten(values: Iterable[Union[Optional[X], List[X], Tuple[X]]]) -> Set[X]:
+    """
+    Flattens non-None values into a set.
+    """
+    out = set()  # type: Set[X]
+
+    for value in values:
+        if value is None:
+            continue
+
+        if isinstance(value, (tuple, list)):
+            out.update(flatten(value))
+            continue
+
+        out.add(value)
+
+    return out
+
+
+print(flatten([2, 5, ['g', 78]]))
+
+
+# 3
+def test2(n: X) -> X:
+    return n
+
+
+def test3(n: X) -> X:
+    return n
+
+
+test2(5)  # OK
+test3("ss")  # OK
+
+
+# 4
+class Test(Generic[X]):
+    def func_1(self, n: X) -> X:
+        return n
+
+    def func_2(self, n: X) -> X:
+        return n
+
+
+t = Test()  # type: Test[int]
+print(t.func_1(1))  # OK
+print(t.func_2('a'))  # mypy error incompatible type "str"; expected "int"
