bfea65f0add9588658c6adf565133544d41dcab6
Yuqing <yuqing@boost.ai>
2020-09-02 12:28:50 +0200

    app docker 20200902

--- apiappdocker/app/app.py
+++ apiappdocker/app/app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 import flask
 
@@ -7,11 +8,20 @@ app = flask.Flask(__name__)
 
 
 COLOR = os.environ.get('COLOR', 'turquoise')
+FILEPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'file.txt'))
 
 
 @app.route('/')
 def index():
     return flask.render_template('index.html', color=COLOR)
+
+
+@app.route('/edit')
+def edit():
+    with open(FILEPATH, 'a') as f:
+        f.write('new line {} \n'.format(datetime.datetime.now()))
+
+    return 'OK'
 
 
 if __name__ == '__main__':
